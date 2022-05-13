b8d3b22e5583a68b2a2992a94831da0cfaeb710b
Alena Nemtsova <jelenatbs@gmail.com>
2022-05-13 18:50:59 +0300

    add icons and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header'
+
 
 function App() {
   return (
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
-import './Header.scss';
+import './Header.css';
+import bookIcon from '../assets/images/book-icon.svg'
+import brainIcon from '../assets/images/brain-icon.svg'
+import light from '../assets/images/lightbulbon.svg'
+
 
 function Header() {
     return (
         <div className='header'>
 
-        <div className='dictionary'>
-            <div className='dictionary__icon'>
-                <img></img>
-            </div>
-            <div className='dictionary__title'></div>
-        </div>
-        <div className='learn'>
-        <div className='learn__icon'>
-                <img></img>
-            </div>
-            <div className='learn__title'></div>
+            <div className='dictionary'>
+                <div className='dictionary__icon'>
+                    <a href='#'><img src={bookIcon} width="45px" height="45px"></img></a>
 
-        </div>
-        <div className='quiz'>
-        <div className='quiz__icon'>
-                <img></img>
+                </div>
+                <div className='dictionary__title'>
+                    <a href='#'>Словарь</a>
+                </div>
             </div>
-            <div className='quiz__title'></div>
+            <div className='learn'>
+                <div className='learn__icon'>
+                    <a href='#'><img src={brainIcon} width="45px" height="45px"></img></a>
+                </div>
+                <div className='learn__title'>
+                    <a href='#'>Учить слова</a>
+                </div>
+
+            </div>
+            <div className='quiz'>
+                <div className='quiz__icon'>
+                    <a href='#'><img src={light} width="45px" height="45px"></img></a>
+                </div>
+                <div className='quiz__title'>
+                    <a href='#'>Проверь себя</a>
+                </div>
 
 
-        </div>
-            
+            </div>
+
         </div>
     )
 }
