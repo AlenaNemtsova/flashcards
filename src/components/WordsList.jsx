cf5e5b0d9cd370b6d690629f2d2c567201d40a42
Alena Nemtsova <jelenatbs@gmail.com>
2022-05-27 14:13:40 +0300

    small changes: App dimentions, row's data

--- src/components/WordsList.jsx
+++ src/components/WordsList.jsx
@@ -6,6 +6,14 @@ import cancelIcon from '../assets/images/cancel2-cross-icon.svg'
 
 const rows = [
     { "id": "11346", "english": "street", "transcription": "[ stri:t ]", "russian": "улица", "tags": "овощи", "tags_json": "[\"u043eu0432u043eu0449u0438\"]" },
+
+    {"id":"11347","english":"car","transcription":"[ kɑ: ]","russian":"автомобиль","tags":"","tags_json":"[\"\"]"},
+
+    {"id":"11350","english":"arm","transcription":"[ ɑ:m ]","russian":"рука","tags":"","tags_json":"[\"\"]"},
+    
+    {"id":"11354","english":"air","transcription":"[  eər ]","russian":"воздух","tags":"","tags_json":"[\"\"]"},
+    
+    {"id":"11355","english":"fox","transcription":"[ fɒks ]","russian":"лиса","tags":"","tags_json":"[\"\"]"},
 ];
 
 
