15d50cf21920f11952b0e9cb134961b6ea678acc
Alena Nemtsova <jelenatbs@gmail.com>
2022-06-06 14:38:13 +0300

    lifting state up to the App, add useState

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import FlashCard from './components/FlashCard';
 import WordsList from './components/WordsList';
 // import './assets/styles/mainStyles.scss'; переменные не подключаются
 
+const rows = [
+  { "id": "11346", "english": "street", "transcription": "[ stri:t ]", "russian": "улица", "tags": "овощи", "tags_json": "[\"u043eu0432u043eu0449u0438\"]" },
 
+  {"id":"11347","english":"car","transcription":"[ kɑ: ]","russian":"автомобиль","tags":"","tags_json":"[\"\"]"},
+
+  {"id":"11350","english":"arm","transcription":"[ ɑ:m ]","russian":"рука","tags":"","tags_json":"[\"\"]"},
+  
+  {"id":"11354","english":"air","transcription":"[  eər ]","russian":"воздух","tags":"","tags_json":"[\"\"]"},
+  
+  {"id":"11355","english":"fox","transcription":"[ fɒks ]","russian":"лиса","tags":"","tags_json":"[\"\"]"},
+];
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
 
-      <FlashCard></FlashCard>
-      <WordsList></WordsList>
+      <FlashCard rows={rows}></FlashCard>
+      <WordsList rows={rows}></WordsList>
       
       <footer>
 
--- src/components/WordsList.jsx
+++ src/components/WordsList.jsx
@@ -4,22 +4,14 @@ import pencilIcon from '../assets/images/pencil5-icon.svg';
 import deleteIcon from '../assets/images/trash-icon.svg';
 import cancelIcon from '../assets/images/cancel2-cross-icon.svg'
 
-const rows = [
-    { "id": "11346", "english": "street", "transcription": "[ stri:t ]", "russian": "улица", "tags": "овощи", "tags_json": "[\"u043eu0432u043eu0449u0438\"]" },
 
-    {"id":"11347","english":"car","transcription":"[ kɑ: ]","russian":"автомобиль","tags":"","tags_json":"[\"\"]"},
+function WordsList({rows}) {
 
-    {"id":"11350","english":"arm","transcription":"[ ɑ:m ]","russian":"рука","tags":"","tags_json":"[\"\"]"},
-    
-    {"id":"11354","english":"air","transcription":"[  eər ]","russian":"воздух","tags":"","tags_json":"[\"\"]"},
-    
-    {"id":"11355","english":"fox","transcription":"[ fɒks ]","russian":"лиса","tags":"","tags_json":"[\"\"]"},
-];
+    const [isEditMode, setIsEditMode] = useState(false);
 
-
-function WordsList() {
-
-    let isEditMode = false;
+    const handleEdit = () => {
+        setIsEditMode(!isEditMode);
+    }
 
     return (
         <React.Fragment>
@@ -37,39 +29,49 @@ function WordsList() {
                     {rows.map((row) =>
                         <tr>
                             <td>
-                                {isEditMode ? <input placeholder='Термин'></input> : row.english}
+                                {isEditMode ? <input type="text" value={row.english}></input> : row.english}
                             </td>
 
                             <td>
-                                {isEditMode ? <input placeholder='Перевод'></input> : row.russian}
+                                {isEditMode ? <input type="text" value={row.russian}></input> : row.russian}
                             </td>
                             <td>
-                                {isEditMode ? <input placeholder='Транскрипция'></input> : row.transcription}
+                                {isEditMode ? <input type="text" value={row.transcription}></input> : row.transcription}
                             </td>
                             <td className='row-control-btns'>
                                 {isEditMode ?
                                     <>
                                         <button className='save-btn'>Сохранить</button>
-                                        <button className='cancel-btn'><img src={cancelIcon}></img></button></> :
+                                        <button className='cancel-btn'><img src={cancelIcon}></img></button>
+                                    </> :
                                     <>
-                                        <button className='edit-btn'><img src={pencilIcon}></img></button>
-                                        <button className='delete-btn'><img src={deleteIcon}></img></button></>}
+                                        <button className='edit-btn' onClick={handleEdit}><img src={pencilIcon}></img></button>
+                                        <button className='delete-btn'><img src={deleteIcon}></img></button>
+                                    </>
+                                }
                             </td>
                         </tr>
                     )
                     }
 
+                    <tr>
+                        <td>
+                        <input placeholder='Термин'></input>
+                        </td>
+                        <td>
+                        <input placeholder='Перевод'></input>
+                        </td>
+                        <td>
+                        <input placeholder='Транскрипция'></input>
+                        </td>
+                        <td>
+                        <button className='save-btn'>Сохранить</button>
+                        </td>
+                    </tr>
 
                 </tbody>
 
-
-
             </table>
-
-            
-
-
-
 
         </React.Fragment>
     )
