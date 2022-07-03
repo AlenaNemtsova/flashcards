54b7fd86fc647de93f53f4d9be82838e58a36983
Alena Nemtsova <jelenatbs@gmail.com>
2022-07-03 11:35:52 +0300

    add default props for the flash card

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,20 +1,16 @@
 import ButtonTranslate from './ButtonTranslate';
 import './FlashCard.scss';
 
-function FlashCard({ ...props}) {
-    FlashCard.defaultProps = {
-        english: "hello",
-        transcription: "[hə'ləʊ]",
-        russian: "привет",
-    }
+function FlashCard({tags = "tag", english = "english word", russian = "russian word", transcription = "transcription", pressed ={pressed}}) {
+    
     
     return (
         <div className='flashcard-wrapper'>
             <div className='flashcard'>
-                <h4 className='flashcard__title'>{props.tags}</h4>
-                <p className='flashcard__source'>{props.english}</p>
-                <p className='flashcard__transcription'>{props.transcription}</p>
-                <ButtonTranslate russian={props.russian}></ButtonTranslate> 
+                <h4 className='flashcard__title'>{tags}</h4>
+                <p className='flashcard__source'>{english}</p>
+                <p className='flashcard__transcription'>{transcription}</p>
+                <ButtonTranslate pressed={pressed} russian={russian}></ButtonTranslate> 
             </div>
 
         </div>
