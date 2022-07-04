baca84f72dacc79ed643c339e5a68f3dadc2e439
Alena Nemtsova <jelenatbs@gmail.com>
2022-07-04 17:49:29 +0300

    add flash card animation

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -7,16 +7,20 @@ import arrowRight from "../assets/images/right-arrow.svg";
 function CardsContainer({ rows }) {
   const [index, setIndex] = useState(0);
   const [pressed, setPressed] = useState(false);
+  const [animationStart, setAnimationStart] = useState(false);
 
   const handleClickForward = () => {
+    setAnimationStart(true);
     setIndex(index >= rows.length - 1 ? 0 : index + 1);
     setPressed(false);
   };
 
   const handleClickBack = () => {
+    setAnimationStart(true);
     setIndex(index <= 0 ? rows.length - 1 : index - 1);
     setPressed(false);
   };
+
 
   return (
     <div className="cards-container">
@@ -32,9 +36,10 @@ function CardsContainer({ rows }) {
         tags={rows[index].tags}
         pressed = {pressed}
         setPressed = {setPressed}
+        animationStart = {animationStart}
       ></FlashCard>
 
-      <button className="forward-btn" onClick={handleClickForward}>
+      <button className="forward-btn" onClick={handleClickForward} onAnimationEnd={() => setAnimationStart(false)}>
         <img src={arrowRight} alt="arrow to flip forward" />
       </button>
     </div>
--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,10 +1,13 @@
 import ButtonTranslate from './ButtonTranslate';
 import './FlashCard.scss';
+import 'animate.css';
 
-function FlashCard({tags = "tag", english = "english word", russian = "russian word", transcription = "transcription", pressed ={pressed}, setPressed = {setPressed}}) {
+function FlashCard({tags = "tag", english = "english word", russian = "russian word", transcription = "transcription", pressed ={pressed}, setPressed = {setPressed}, animationStart}) {
+
+    console.log(animationStart);
 
     return (
-        <div className='flashcard-wrapper'>
+        <div className={(animationStart ? "flashcard-wrapper animated" : "flashcard-wrapper")}>
             <div className='flashcard'>
                 <h4 className='flashcard__title'>{tags}</h4>
                 <p className='flashcard__source'>{english}</p>
