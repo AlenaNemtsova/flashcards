4b0a4ade3ff92dcaa6ce8ff402ff0b212a4f5ae0
Alena Nemtsova <jelenatbs@gmail.com>
2022-12-08 11:56:05 +0300

    changed defaultProps syntax

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -4,32 +4,12 @@ import 'animate.css';
 // import { useRef, useEffect } from 'react';
 import { forwardRef } from 'react';
 
-// function FlashCard({tags = "tag", english = "english word", russian = "russian word", transcription = "transcription", pressed ={pressed}, setPressed = {setPressed}, animationStart, countWords={countWords}}) {
 
-//     console.log(animationStart);
-
-//     const ref = useRef();
-//     useEffect(() => ref.current.focus(), [])
-
-//     return (
-//         <div className={(animationStart ? "flashcard-wrapper animated" : "flashcard-wrapper")}>
-//             <div className='flashcard'>
-//                 <h4 className='flashcard__title'>{tags}</h4>
-//                 <p className='flashcard__source'>{english}</p>
-//                 <p className='flashcard__transcription'>{transcription}</p>
-//                 <ButtonTranslate pressed={pressed} setPressed = {setPressed} countWords={countWords} russian={russian} ref={ref}></ButtonTranslate> 
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default FlashCard
-
-
-const FlashCard = forwardRef(({tags = "tag", english = "english word", russian = "russian word", transcription = "transcription", pressed ={pressed}, setPressed = {setPressed}, animationStart, countWords={countWords}}, ref) => {
+const FlashCard = forwardRef(({ tags, english, russian, transcription, pressed = { pressed }, setPressed = { setPressed }, animationStart, countWords = { countWords } }, ref) => {
 
     console.log(animationStart);
+    //     const ref = useRef();
+    //     useEffect(() => ref.current.focus(), [])
 
     return (
         <div className={(animationStart ? "flashcard-wrapper animated" : "flashcard-wrapper")}>
@@ -37,13 +17,20 @@ const FlashCard = forwardRef(({tags = "tag", english = "english word", russian =
                 <h4 className='flashcard__title'>{tags}</h4>
                 <p className='flashcard__source'>{english}</p>
                 <p className='flashcard__transcription'>{transcription}</p>
-                <ButtonTranslate pressed={pressed} setPressed = {setPressed} countWords={countWords} russian={russian} ref={ref}></ButtonTranslate> 
+                <ButtonTranslate pressed={pressed} setPressed={setPressed} countWords={countWords} russian={russian} ref={ref}></ButtonTranslate>
             </div>
 
         </div>
     );
 },
 )
+
+FlashCard.defaultProps = {
+    tags: "tag",
+    english: "english word",
+    russian: "russian word",
+    transcription: "transcription"
+}
 
 export default FlashCard
 
