7fc59f4a0ee223a0d2804a4b1fce6f694be1de28
Alena Nemtsova <jelenatbs@gmail.com>
2022-12-26 19:22:45 +0300

    use BrowserRouter here

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import App from './pages/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
