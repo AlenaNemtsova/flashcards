1a80d7261321a4a46399217a950f09165e7c7e54
Alena Nemtsova <jelenatbs@gmail.com>
2022-12-26 19:23:00 +0300

    move to pages

---
+++ src/pages/App.js
@@ -0,0 +1,26 @@
+import Header from '../components/Header/Header';
+import Card from '../components/Card/Card';
+import WordsList from './WordsList';
+import CardsContainer from './CardsContainer';
+import NoMatch from './NoMatch';
+import { Routes, Route } from 'react-router-dom';
+import '../styles/App.css';
+import '../styles/scss/styles.scss'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="header" />
+      <Routes>
+        <Route path='/' element={<WordsList />} />
+        <Route path='/dictionary' element={<WordsList />} />
+        <Route path='/learn' element={<CardsContainer className="cards" ><Card /></CardsContainer>} />
+        <Route path='/quiz' />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
